9bb781012e03adcf730ef493aed4f1955af0676b
Sivin Saji <sivinsaji22@gmail.com>
2022-09-22 22:58:09 +0530

    Mongodb Connected

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var hbs = require('express-handlebars');
+var db = require('./config/connection')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,6 +21,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Database connection message
+db.connect((err) => {
+  if (err) console.log('Connection Error: ' + err)
+  else console.log('Database Connected to port 27017')
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
