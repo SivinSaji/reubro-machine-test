4072f5d8bb60f336c974bc1b9c67c41d5deb4a5a
Sivin Saji <sivinsaji22@gmail.com>
2022-09-22 11:44:06 +0530

    Designed login,signup,home,edituser pages and added bootstrap link to main.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hbs = require('express-handlebars');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,7 +13,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
+//Here we set layout and partials
+app.engine('hbs', hbs.engine({ extname: 'hbs', defautLayout: 'layout', layoutsDir: __dirname + '/views/layout', partialsDir: __dirname + '/views/partials/'}))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', {});
 });
 
+/* GET home page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', {});
+});
+
+
+/* GET home page. */
+router.get('/signup', function(req, res, next) {
+  res.render('signup', {});
+});
+
+/* GET home page. */
+router.get('/edituser', function(req, res, next) {
+  res.render('edituser', {});
+});
 module.exports = router;
